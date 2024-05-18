b29f9e37ea6ec6e6fdb56af658bd3191c589aa73
borysigorevich <borysigorevich@gmail.com>
2024-05-18 13:26:22 +0300

    add prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <main className="bg-slate-50 h-full">
-    </main>
-  );
+	return <main className="bg-slate-50 h-full"></main>;
 }
--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import { cn } from '@/lib/utils';
+import React, { ReactNode } from 'react';
 
-export const MaxWidthWrapper = () => {
- return (
-  <div>
-
-  </div>
- );
+export const MaxWidthWrapper = ({
+	children,
+	className,
+}: {
+	children: ReactNode;
+	className?: string;
+}) => {
+	return (
+		<div className={cn('max-w-xl h-full w-full mx-auto px-2.5 md:px-20', className)}>
+			{children}
+		</div>
+	);
 };
